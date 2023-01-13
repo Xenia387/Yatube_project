c4abd5ae80b528af31309deee0372b178ee812be
Ксения Анисимова <anis.xenia@yandex.ru>
2023-01-13 03:36:21 +0300

    внесение изменений

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,6 +3,7 @@ from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
+    list_editable = ('group',)
     list_display = (
         'pk',
         'text',
@@ -10,22 +11,10 @@ class PostAdmin(admin.ModelAdmin):
         'author',
         'group',
     )
-    list_editable = ('group',)
-    search_fields = ('text',)
-    list_filter = ('pub_date',)
-    empty_value_display = '-пусто-'
-
-
-class GroupAdmin(admin.ModelAdmin):
-    list_display = (
-        'title',
-        'slug',
-        'description',
-    )
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group, GroupAdmin)
+admin.site.register(Group)
